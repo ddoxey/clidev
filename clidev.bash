6b2e17792259600bc2939ea3d2cae1d975075098
Dylan Doxey <dylan.doxey@gmail.com>
2023-02-25 05:36:51 +0000

    Add list, permute; improve vim

--- clidev.bash
+++ clidev.bash
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-function highlight()
+# clear and print a page of newlines
+function cls()
 {
-    local pattern="$(sed -e 's|\([+/()|]\)|\\\1|g' <<< "$@")"
-    local on='\\\\033[32m'
-    local off='\\\\033[0m'
-    sed -e "s|'|\\\'|g" -e "s/\($pattern\)/${on}\1${off}/g" | xargs echo -e
+    clear
+    for n in $(seq 1 $(tput lines)); do echo; done
 }
 
+# highlight lines on STDIN with the given regex
+function highlight()
+{
+    if [[ $# -eq 0 ]]
+    then
+        cat
+    else
+        local pattern="$(sed -e 's|\([+/()|]\)|\\\1|g' <<< "$@")"
+        local on='\\\\033[32m'
+        local off='\\\\033[0m'
+        sed -e "s|'|\\\'|g" -e "s/\($pattern\)/${on}\1${off}/g" | xargs echo -e
+    fi
+}
+
+# number lines and add them to the ~/.num database
 function num()
 {
     if [[ $# -eq 0 ]]
@@ -38,26 +52,7 @@ function num()
     fi
 }
 
-
-function vim()
-{
-    if [[ $# -eq 1 ]] && [[ "$1" =~ ^[0-9]+$ ]]
-    then
-        local n
-        local filename="$(num $1)"
-        if [[ "$filename" =~ [:] ]]
-        then
-            n="+$(awk -F: '{print $2}' <<< "$filename")"
-            filename="$(awk -F: '{print $1}' <<< "$filename")"
-        fi
-        history -s vim $n "$filename"
-        /usr/bin/vim $n "$filename"
-    else
-        /usr/bin/vim "$@"
-    fi
-}
-
-
+# find text files
 function findf()
 {
     local where ext
@@ -83,9 +78,51 @@ function findf()
     fi
 }
 
+# wrapper for /usr/bin/vim opens numbered files
+function vim()
+{
+    if [[ $# -eq 1 ]]
+    then
+        local filename="$1"
+        if [[ "$filename" =~ ^[0-9]+$ ]]
+        then
+            local n
+            filename="$(num $1)"
+            if [[ "$filename" =~ [:] ]]
+            then
+                n="+$(awk -F: '{print $2}' <<< "$filename")"
+                filename="$(awk -F: '{print $1}' <<< "$filename")"
+            fi
+        elif [[ ! "$filename" =~ [/] ]] && [[ ! -e "$filename" ]]
+        then
+            if [[ "$filename" =~ [.] ]]
+            then
+                local found="$(find . -type f -name "$filename" 2>/dev/null | head -n 1)"
+                if [[ -n $found ]]
+                then
+                    filename="$(sed 's|^[.]/||' <<< "$found")"
+                fi
+            fi
+        fi
+        if [[ "${filename:0:1}" == "/" ]]
+        then
+            filename="$(sed "s|$(pwd)/||" <<< "$filename")"
+        fi
+        history -s vim $n "$filename"
+        /usr/bin/vim $n "$filename"
+    else
+        /usr/bin/vim "$@"
+    fi
+}
 
+# search for files containing the given pattern
 function search()
 {
+    if [[ $# -lt 1 ]]
+    then
+        echo "search <regex> [<ext>]" >&2
+        return 1
+    fi
     local pattern="$1"
     local ext="$2"
     local wd="$(pwd)"
@@ -110,4 +147,72 @@ function search()
             done <<< "$(egrep -Hn "$pattern" "$filename")"
         fi
     done <<< "$(findf "$ext" | sed "s|^[.]/|$wd/|")"
+}
+
+# enumerate all permutations of the given tokens
+function permute()
+{
+    if [[ $# -eq 0 ]]
+    then
+        echo "permute <token 1> <token 2> [... <token 8>]" >&2
+        return 1
+    elif [[ $# -eq 1 ]]
+    then
+        echo "$1"
+        return
+    elif [[ $# -gt 8 ]]
+    then
+        echo "$(basename "$0"): No more than 8 tokens" >&2
+        return 1
+    fi
+
+    local token="$1"
+    shift
+    local p_len=$#
+
+    while IFS="\n" read -r permutation
+    do
+        local ptokens=($permutation)
+        local k=0
+
+        for i in $(seq 0 $p_len)
+        do
+            for j in $(seq 0 $p_len)
+            do
+                if [[ $j -eq $k ]]
+                then
+                    echo -n " $token";
+                fi
+                if [[ -n ${ptokens[$j]} ]]
+                then
+                    echo -n " ${ptokens[$j]}"
+                fi
+            done
+            echo
+            let k=k+1
+        done
+    done <<< "$(permute "$@")" | sed 's/ //'
+}
+
+# list matching files with a recursive search
+function list()
+{
+    if [[ $# -eq 0 ]]
+    then
+        echo "list <token> [<token> ...]" >&2
+        return 1
+    fi
+
+    permute "$@" | \
+        awk -v q="'" '{
+            gsub(/ /, "*", $0)
+            find = "find . -type f -iname " q "*" $0 "*" q " 2>/dev/null"
+            find | getline filename
+            close(find)
+            if (filename) {
+                gsub(/^[.][/]/, "", filename)
+                if (++count[filename] == 1) print filename
+            }
+        }' | \
+        num
 }
